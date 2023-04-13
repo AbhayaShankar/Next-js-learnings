43daf563aff310688879e8a5d29da752573fe5e8
Shanky <fransisagenta@gmail.com>
2023-04-13 18:18:18 +0530

    done next first project

--- project-1/pages/events/[...slug].jsx
+++ project-1/pages/events/[...slug].jsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../Dummydata";
+import EventList from "../../components/events/EventList";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -7,15 +9,49 @@ function FilteredEventsPage() {
   console.log(filterData);
 
   if (!filterData) {
-    return <p>Loading...</p>;
+    return <p className="center">Loading...</p>;
   }
 
   const filterYear = filterData[0];
   const filterMonth = filterData[1];
 
+  const numYear = +filterYear;
+  const numMonth = +filterMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2028 ||
+    numYear < 2020 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return (
+      <p className="center">
+        Invalid Filter... Please enter Year below 2028 and after 2020...
+      </p>
+    );
+  }
+
+  const filterEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  // For events not found in the data, I would want to return a message for events not found in the database or no proper match for the filter applied.
+
+  if (!filterEvents || filterEvents.length === 0) {
+    return (
+      <p className="center">
+        events not found in the database or no proper match for the filter
+        applied.
+      </p>
+    );
+  }
+
   return (
     <div>
-      <h1>Filtered Event Page</h1>
+      <EventList featuredEvents={filterEvents} />
     </div>
   );
 }
