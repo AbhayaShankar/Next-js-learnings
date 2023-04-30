36cb0391c4d462668bf12e641c7a84cef8cab643
Abhaya <shankarabhaya9@gmail.com>
2023-05-01 00:48:02 +0530

    API routes started

--- API Routes/pages/index.js
+++ API Routes/pages/index.js
@@ -1,7 +1,33 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    console.log(enteredEmail, enteredFeedback);
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Submit your Feedback</button>
+      </form>
     </div>
   );
 }
