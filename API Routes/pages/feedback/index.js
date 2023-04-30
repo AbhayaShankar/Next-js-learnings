c183f3da65d39488dd5cbfdc45d520f27646c791
Abhaya <shankarabhaya9@gmail.com>
2023-05-01 03:23:26 +0530

    fetching data from api inside our api routes

--- API Routes/pages/feedback/index.js
+++ API Routes/pages/feedback/index.js
@@ -10,6 +10,8 @@ function FeedbackPage({ feedbackItems }) {
   );
 }
 
+// We should use fetch inside getStaticProps or getServerSideProps for fetching our own api... instead we should use our existing api to reuse and display data. SInce all api are running on the same server.
+
 export async function getStaticProps() {
   const filePath = buildFeedbackPath();
   const data = extractFeedback(filePath);
