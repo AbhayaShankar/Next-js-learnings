8ed9145fb47ab9f6479a96927ea25b66884bf2f8
Abhaya <shankarabhaya9@gmail.com>
2023-05-02 15:43:00 +0530

    context api in NEXT

--- project-1/pages/_app.js
+++ project-1/pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from "../components/layout/layout";
+import Notification from "../components/notification/notification";
 import "../styles/globals.css";
 import Head from "next/head";
 
@@ -9,6 +10,11 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification
+        title={"Comments Submitted"}
+        message={"Comments successfully submitted to MongoDB"}
+        status={"failed"}
+      />
     </Layout>
   );
 }
