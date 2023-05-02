e16d15a0b068423d200ce6dd9168c3673c5f238e
Abhaya <shankarabhaya9@gmail.com>
2023-05-03 01:19:38 +0530

    hero section update

--- Blog-Next-App/pages/index.jsx
+++ Blog-Next-App/pages/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Hero from "../components/HomePage/Hero";
+import FeaturedPosts from "../components/HomePage/FeaturedPosts";
 
 const HomePage = () => {
   return (
-    // This Home Page will have 2 main sections:
-    // 1. The Hero Section...
-    // 2. The Featured Posts section. - For now we can show some dummy data, then we can move forward with linking in the future.
-    <div>
-      <p>Abhaya</p>
-    </div>
+    <Fragment>
+      {/*  
+      This Home Page will have 2 main sections:
+         1. The Hero Section...
+         2. The Featured Posts section. - For now we can show some dummy data, then we can move forward with linking in the future.
+     */}
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
   );
 };
 
