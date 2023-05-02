62a4d1e98440e66831e5a53ca50fb7b953a01b1b
Abhaya <shankarabhaya9@gmail.com>
2023-05-03 03:20:11 +0530

    Navbar designed

--- Blog-Next-App/components/layout/Navbar.jsx
+++ Blog-Next-App/components/layout/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className={classes.header}>
       <div className={classes.left_navbar}>
-        <p>Abhaya's Blog</p>
+        <p>
+          <Link href="/">Abhaya's Blog</Link>
+        </p>
       </div>
-      <div className={classes.right_navbar}>
+      <nav className={classes.right_navbar}>
         <ul className="">
-          <li>Home</li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
           <li>Projects</li>
-          <li>Blogs</li>
-          <li>Contact</li>
+          <li>
+            <Link href="/posts">Blogs</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
-      </div>
+      </nav>
     </header>
   );
 };
