8b30f284d71f34f1a727727cbdcf357f9077e489
Abhaya <shankarabhaya9@gmail.com>
2023-05-05 00:34:44 +0530

    basic form added

--- Blog-Next-App/pages/contact.jsx
+++ Blog-Next-App/pages/contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ContactForm from "../components/contact/ContactForm";
 
 const ContactPage = () => {
-  return <div>contact</div>;
+  return <ContactForm />;
 };
 
 export default ContactPage;
