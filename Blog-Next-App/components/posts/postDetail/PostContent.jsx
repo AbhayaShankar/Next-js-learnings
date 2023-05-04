22ee27966f9393e352f4ee4e0c03b02fce56364e
Abhaya <shankarabhaya9@gmail.com>
2023-05-04 19:50:38 +0530

    eror with customrenderers

--- Blog-Next-App/components/posts/postDetail/PostContent.jsx
+++ Blog-Next-App/components/posts/postDetail/PostContent.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./PostHeader";
 import classes from "./PostContent.module.css";
-import { DUMMY_POSTS } from "../../../helpers/DummySinglePosts";
+import Image from "next/image";
 
 const PostContent = ({ post }) => {
   const { title, image, content, slug } = post;
 
   const imagePath = `/posts/${slug}/${image}`;
+
+  console.log(slug);
+
+  const customRenderers = {
+    image(image) {
+      return (
+        <Image
+          src={`/posts/${slug}/${image.src}`}
+          alt={image.alt}
+          width={300}
+          height="auto"
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 };
